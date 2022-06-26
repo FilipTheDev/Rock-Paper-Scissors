de907e0792f38876f4dfa773a0bf8233aef7f39a
Filip Tomevski <ftomevski16@gmail.com>
2022-06-26 02:11:38 +0200

    Fixed my error with the for loop. Now it's not showing points on each round

--- script.js
+++ script.js
@@ -19,40 +19,40 @@ function gameRound(playerSelection, computerSelection) {
     if(playerSelection == computerSelection) {
         if(playerSelection == "rock") {
             console.log("It's a tie, you both chose rock");
-            return "2"
+            return 2
         }
         if(playerSelection == "paper") {
             console.log("It's a tie, you both chose paper");
-            return "2"
+            return 2
         }
         if(playerSelection == "scissors") {
             console.log("It's a tie, you both chose scissors");
-            return "2"
+            return 2
         }
     }
     if(playerSelection == "rock" && computerSelection == "paper") {
         console.log("Computer won, paper beats rock");
-        return "1";
+        return 1;
     }
     if(playerSelection == "rock" && computerSelection == "scissors") {
         console.log("Player won, rock beats scissors");
-        return "0";
+        return 0;
     }
     if(playerSelection == "paper" && computerSelection == "rock") {
         console.log("Player won, paper beats rock");
-        return "0";
+        return 0;
     }
     if(playerSelection == "paper" && computerSelection == "scissors") {
         console.log("Computer won, scissors beats paper");
-        return "1";
+        return 1;
     }
     if(playerSelection == "scissors" && computerSelection == "rock") {
         console.log("Computer won, rock beats scissors");
-        return "1";
+        return 1;
     }
     if(playerSelection == "scissors" && computerSelection == "paper") {
         console.log("Player won, scissors beats paper");
-        return "0";
+        return 0;
     }
     else {
         return false;
@@ -62,15 +62,21 @@ function gameRound(playerSelection, computerSelection) {
 function game() {
     let playerPoints = 0;
     let compPoints = 0;
-    for(let i = 0; i < 1; i++) {
+    for (let i = 0; i < 5; i++) {
         gameRound();
-        if(gameRound() == 2) {
+        //If the function has the value of 2 it means the round was a tie
+        if(gameRound == 2) {
+            console.log("Player: " + playerPoints + " Computer: " + compPoints);
         }
-        if(gameRound() == 0) {
+        //If the function has the value of 0 it means in the round player won
+        if(gameRound == 0) {
             playerPoints++;
+            console.log("Player: " + playerPoints + " Computer: " + compPoints);
         }
-        if(gameRound() == 1) {
+        //If the function has the value of 1 it means in the round computer won
+        if(gameRound == 1) {
             compPoints++;
+            console.log("Player: " + playerPoints + " Computer: " + compPoints);
         }
     }
     if(playerPoints == compPoints) {
