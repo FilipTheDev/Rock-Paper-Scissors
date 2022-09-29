dfe72b214da88cdb9afbcb043b9872200883112e
Filip Tomevski <ftomevski16@gmail.com>
2022-09-30 00:44:20 +0200

    Finished scoreboard and end of game. Everything is with UI

--- script.js
+++ script.js
@@ -12,81 +12,76 @@ function computerPlay() {
 }
 var selBut = document.getElementsByClassName("selBut");
 var displayResDiv = document.querySelector("#displayRes");
+const rock = document.getElementById("rock");
+const paper = document.getElementById('paper');
+const scissors = document.getElementById('scissors');
+var h2 = document.getElementById('h2');
+var h3 = document.getElementById('h3');
+
+
+let playerPoints = 0;
+let compPoints = 0;
 
 function gameRound(playerSelection, computerSelection) {    
-    // let playerInput = prompt("Do you choose rock, paper or scissors?");
+    
     computerSelection = computerPlay();
+    
+    // Ouputs of a tie
     if(playerSelection == computerSelection) {
         if(playerSelection == "rock") {
             displayResDiv.textContent = "It's a tie, you both chose rock";
-            return 2
+
         }
         if(playerSelection == "paper") {
             displayResDiv.textContent = "It's a tie, you both chose paper";
-            return 2
+
         }
         if(playerSelection == "scissors") {
             displayResDiv.textContent = "It's a tie, you both chose scissors";
-            return 2
+
         }
     }
+    // Outputs of winning and losing
+
     if(playerSelection == "rock" && computerSelection == "paper") {
         displayResDiv.textContent = "Computer won, paper beats rock";
-        return 1;
+        compPoints++;
     }
     if(playerSelection == "rock" && computerSelection == "scissors") {
         displayResDiv.textContent = "Player won, rock beats scissors";
-        return 0;
+        playerPoints++;
     }
     if(playerSelection == "paper" && computerSelection == "rock") {
         displayResDiv.textContent = "Player won, paper beats rock";
-        return 0;
+        playerPoints++;
     }
     if(playerSelection == "paper" && computerSelection == "scissors") {
         displayResDiv.textContent = "Computer won, scissors beats paper";
-        return 1;
+        compPoints++;
+  
     }
     if(playerSelection == "scissors" && computerSelection == "rock") {
         displayResDiv.textContent = "Computer won, rock beats scissors";
-        return 1;
+        compPoints++;
     }
     if(playerSelection == "scissors" && computerSelection == "paper") {
         displayResDiv.textContent = "Player won, scissors beats paper";
-        return 0;
+        playerPoints++;
     }
-    else {
-        return false;
+    h2.textContent = ("Player: " + playerPoints + " Computer: " + compPoints);
+    
+    if(playerPoints == 5 || compPoints == 5) {
+        rock.disabled = true;
+        paper.disabled = true;
+        scissors.disabled = true;
+        if(playerPoints < compPoints) {
+            alert("Computer won!")
+            h3.textContent = "You lost! *wah wah*"
+        }
+        if(playerPoints > compPoints) {
+            alert("Player won!")
+            h3.textContent = "Congratulations, you won! Now go touch some grass"
+        }
+        
     }
 }
-
-function game() {
-    let playerPoints = 0;
-    let compPoints = 0;
-    for (let i = 0; i < 5; i++) {
-        let roundWin = gameRound();
-        //If the function has the value of 2 it means the round was a tie
-        if(roundWin == 2) {
-            console.log("Player: " + playerPoints + " Computer: " + compPoints);
-        }
-        //If the function has the value of 0 it means in the round player won
-        if(roundWin == 0) {
-            playerPoints++;
-            console.log("Player: " + playerPoints + " Computer: " + compPoints);
-        }
-        //If the function has the value of 1 it means in the round computer won
-        if(roundWin == 1) {
-            compPoints++;
-            console.log("Player: " + playerPoints + " Computer: " + compPoints);
-        }
-    }
-    if(playerPoints == compPoints) {
-        console.log("It's a tie!")
-    }
-    if(playerPoints < compPoints) {
-        console.log("Computer won!")
-    }
-    if(playerPoints > compPoints) {
-        console.log("Player won!")
-    }
-}
-
